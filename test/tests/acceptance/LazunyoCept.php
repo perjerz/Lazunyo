5f6e3aea8fe3bbffce45d793cecd1b37426f9aa4
StGrandZ <thummarut.c@gmail.com>
2016-05-18 13:56:00 +0700

    edit

--- test/tests/acceptance/LazunyoCept.php
+++ test/tests/acceptance/LazunyoCept.php
@@ -7,19 +7,22 @@ $I->seeCurrentUrlEquals('/form-login.html');
 function test_login($I) {
 	// logging in
 	$I->amOnPage('/form-login.html');
+	$I->fillField('email','test@test.test');
+	$I->fillField('password','test');
+	$I->click('/html/body/div/form/div[1]/div/div[4]/button');
+	$I->wait(2);
+	$I->seeCurrentUrlEquals('/show-product.php');
+}
+
+function test_addItem($I) {
+	// adding item
+	$I->amOnPage('/show-product.php');
+	$I->fillField('','');
+	$I->fillField('','');
 	$I->fillField('','');
 	$I->fillField('','');
 	$I->click('');
 	$I->seeCurrentUrlEquals('');
 }
 
-function test_addItem($I) {
-	// adding item
-	$I->amOnPage('/add-product.php');
-	$I->fillField('','');
-	$I->fillField('','');
-	$I->fillField('','');
-	$I->fillField('','');
-	$I->click('');
-	$I->seeCurrentUrlEquals('');
-}
+test_login($I);
