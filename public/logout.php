b37df9150a098082b8c23c4d9a84fb0c8195e11e
StGrandZ <thummarut.c@gmail.com>
2016-05-19 15:36:58 +0700

    Log out redirect to index

--- public/logout.php
+++ public/logout.php
@@ -3,5 +3,5 @@ session_start();
 session_unset(); 
 // destroy the session 
 session_destroy(); 
-echo "<script type='text/javascript'>alert('You have logged out.');window.location.href = 'form-login.html';</script>";
+echo "<script type='text/javascript'>alert('You have logged out.');window.location.href = 'index.html';</script>";
 ?>
